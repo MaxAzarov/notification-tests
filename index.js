a8f25a581bb48315022c099c53cefc8ea12b91ec
Maksym Azarov <maksym.azarov@eliftech.com>
2023-01-16 20:45:48 +0200

    check notifications abilities

--- index.js
+++ index.js
@@ -6,17 +6,29 @@ btn.addEventListener("click", () => {
 
 function notifyMe() {
   console.log("here");
-  const notification = new Notification("First notification!");
+  const notification = new Notification("First notification!", {
+    data: {
+      url: "index2.html",
+    },
+  });
 
   if (!("Notification" in window)) {
     alert("This browser does not support desktop notification");
   } else if (Notification.permission === "granted") {
-    const notification = new Notification("Hi there!");
+    const notification = new Notification("Hi there!", {
+      data: {
+        url: "index2.html",
+      },
+    });
   } else if (Notification.permission !== "denied") {
     Notification.requestPermission(function (permission) {
       console.log("🚀 ~ file: index.js:10 ~ permission", permission);
       if (permission === "granted") {
-        const notification = new Notification("Hi there!");
+        const notification = new Notification("Hi there!", {
+          data: {
+            url: "index2.html",
+          },
+        });
       }
     });
   }
@@ -28,7 +40,11 @@ Notification.requestPermission(function (result) {
   if (result === "granted") {
     navigator.serviceWorker.ready
       .then(function (registration) {
-        registration.showNotification("Notification with ServiceWorker");
+        registration.showNotification("Notification with ServiceWorker", {
+          data: {
+            url: "index2.html",
+          },
+        });
       })
       .catch((e) => console.log(e));
   }
--- sw.js
+++ sw.js
@@ -1,17 +1,46 @@
-console.log("Service Worker");
+console.log("Service Worker", self);
+
+self.addEventListener("load", () => {
+  console.log("load");
+});
+
+self.addEventListener("install", (event) => {
+  console.log("install", event);
+  // event.waitUntil(
+  //   addResourcesToCache([
+  //     "/",
+  //     "/index.html",
+  //     "/style.css",
+  //     "/app.js",
+  //     "/image-list.js",
+  //     "/star-wars-logo.jpg",
+  //     "/gallery/bountyHunters.jpg",
+  //     "/gallery/myLittleVader.jpg",
+  //     "/gallery/snowTroopers.jpg",
+  //   ])
+  // );
+});
 
 self.addEventListener("notificationclick", (event) => {
+  console.log("🚀 ~ file: sw.js:4 ~ self.addEventListener ~ event", event);
   const rootUrl = new URL("/", location).href;
+  console.log("🚀 ~ file: sw.js:27 ~ self.addEventListener ~ rootUrl", rootUrl);
   event.notification.close();
   // Enumerate windows, and call window.focus(), or open a new one.
-  event.waitUntil(
-    clients.matchAll().then((matchedClients) => {
-      for (let client of matchedClients) {
-        if (client.url === rootUrl) {
-          return client.focus();
-        }
-      }
-      return clients.openWindow("/");
-    })
-  );
+  // event.waitUntil(
+  //   clients.matchAll().then((matchedClients) => {
+  //     for (let client of matchedClients) {
+  //       if (client.url === rootUrl) {
+  //         return client.focus();
+  //       }
+  //     }
+  //     return clients.openWindow("/");
+  //   })
+  // );
+
+  if (clients.openWindow && event.notification.data.url) {
+    event.waitUntil(clients.openWindow(event.notification.data.url));
+  }
+
+  // return clients.openWindow("/index2.html");
 });
